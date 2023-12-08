97f1d378361004f8b1a9efc1ecb33435bb924484
aribakha <140427904+aribakha@users.noreply.github.com>
2023-12-08 15:17:23 +0500

    pdf assignment

---
+++ app.js
@@ -0,0 +1,109 @@
+                                             // CHAPTER # 21-25
+
+                                               // Question # 1
+
+// var firstName = prompt("Enter your first name :");
+// var lastName = prompt("Enter your last name :");
+// var mergeName = firstName + " " + lastName;
+// alert(" WELCOME ! " + mergeName );
+
+                                               // Question # 2
+
+// var mobileModel = prompt("Enter a any mobile model : ");
+// var mystringLength = mobileModel.length;
+// document.write("My favourite phone is : " + mobileModel + "</br>" );
+// document.write("Length of string : " + mystringLength);
+
+                                               // Question # 3
+
+// var mystring = "Pakistan";
+// var indexN = mystring.indexOf('n');
+// document.write("String : " + mystring + "</br>");
+// document.write("index of 'n' : " + indexN);                                            
+
+                                               // Question # 4
+
+// var mystring = "Hello world";
+// var indexL = mystring.lastIndexOf('l');
+// document.write("String : " + mystring + "</br>");
+// document.write("Index of 'L' : " + indexL + "</br>");                                               
+
+                                               // Question # 5
+
+// var mystring =  "Pakistani";
+// var thirdCharacterFind = mystring.charAt(3);
+// document.write("String : " + mystring + "</br>");
+// document.write("Character in index 3 : " + thirdCharacterFind);  
+
+                                               // Question # 6
+
+// var originalString = " Hyderabad ";
+// var replacedString = originalString.replace(" Hyderabad " , " Islamabad ");
+// document.write("City : " + originalString + "</br>");
+// document.write("After replacement : " + replacedString);                                                
+
+                                               // Question # 7
+
+// var message = " Ali & Sami are best friends.They play cricket & football together."; 
+// var replacement = message.replace(" & " , " and ");
+// document.write( replacement );                                                
+
+                                               // Question # 8
+
+// var string = prompt("Enter any number");
+// var number = parseInt(string);
+// document.write("Value : " + string + "</br>");
+// // document.write("Type : " + string + "</br>");
+// // document.write("Value : " + number + "</br>");
+// // document.write("Type : " + number + "</br>"); 
+
+                                               // Question # 9
+
+// var number = "35.46";
+// var removeDot = number.includes(".");
+// document.write("Number : " + number + "</br>" );
+// document.write(" Replace Dot : " + removeDot);                                              
+
+                                               // Question # 10
+
+// var  user = prompt("Enter any word here : ");
+// var upperCase = user.toUpperCase();
+// document.write("User input : " + user + "</br>");
+// document.write("Upper Case : " + upperCase );
+
+                                               // Question # 11
+
+// var user = prompt("Enter any username:");
+// var save = user;
+// var symbol = "@" ;
+// if ( user === symbol ){
+//   alert("Please enter a valid username ! ")
+// }
+// document.write("Userame : "  + user)  
+
+                                               // Question # 12
+
+// var mystring = " Pakistan";
+// var indexN = mystring.charAt( mystring.length - 1 );
+// document.write("User input : " + mystring + "</br>");
+// document.write("Last character of input 'n' : " + indexN + "</br>")      
+
+                                               // Question # 13
+
+
+// var message = " The quick brown fox jumps over the lazy dog";
+// var lowerCaseSentence = sentence.toLocaleLowerCase();
+// var words = lowerCaseSentence.split( " " );
+// var count = 0 ;
+// for ( var i = 0; i < words.length; i++){
+//   if(words[i] === "the"){
+//     count++;
+//   }
+// }
+// document.write("Text : " + message  + "</br>")
+// document.write(count)
+
+
+
+
+
